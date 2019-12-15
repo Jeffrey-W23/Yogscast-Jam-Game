5931bdc58621a2d790507d59307938912879593d
dodecamon <44084222+dodecamon@users.noreply.github.com>
2019-12-15 00:04:24 -0800

    Finished scene, item sprites, player animation

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("isMove", true);
+        }
+        else
+        {
+            anim.SetBool("isMove", false);
+        }
+	}
+}
